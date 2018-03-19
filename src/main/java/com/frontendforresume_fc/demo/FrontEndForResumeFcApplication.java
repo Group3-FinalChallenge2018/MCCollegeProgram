0c8ce62dd297746a1ed2bed93c2aebdf98a9e106
GBTC406014UR <GBTC406014UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-19 12:11:46 -0400

    Initial commit

---
+++ src/main/java/com/frontendforresume_fc/demo/FrontEndForResumeFcApplication.java
@@ -0,0 +1,12 @@
+package com.frontendforresume_fc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndForResumeFcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndForResumeFcApplication.class, args);
+	}
+}
