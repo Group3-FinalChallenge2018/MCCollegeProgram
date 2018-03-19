52bcf591925192a73448f5651009751ab106e09f
Yool-bin Um <yoolbin.um@gmail.com>
2018-03-19 15:35:24 -0400

    Created Requirement and Programme Models and data loader

---
+++ src/main/java/com/frontendforresume_fc/demo/repository/ProgrammeRepository.java
@@ -0,0 +1,7 @@
+package com.frontendforresume_fc.demo.repository;
+
+import com.frontendforresume_fc.demo.model.Programme;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProgrammeRepository extends CrudRepository<Programme, Long> {
+}
