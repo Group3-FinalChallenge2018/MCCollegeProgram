9fc113d74f589d106a1ae779c40a95f3b4c3d98a
Yool-bin Um <yoolbin.um@gmail.com>
2018-03-23 16:38:31 -0400

    Sending email works Example can be seen under AdminController Make sure you re-import Maven. Also make sure you match destination email and email owner's name
    Our sender email is:
    javabootcamp2018@gmail.com
    Java2018

--- src/main/java/com/frontendforresume_fc/demo/controller/AdminController.java
+++ src/main/java/com/frontendforresume_fc/demo/controller/AdminController.java
@@ -6,11 +6,18 @@ import com.frontendforresume_fc.demo.service.AdminService;
 import com.frontendforresume_fc.demo.service.ProgrammeService;
 import com.frontendforresume_fc.demo.service.StudentService;
 import com.frontendforresume_fc.demo.service.UserService;
+import com.google.common.collect.Lists;
+import it.ozimov.springboot.mail.model.Email;
+import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
+import it.ozimov.springboot.mail.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.mail.internet.InternetAddress;
+import java.io.UnsupportedEncodingException;
 
 @Controller
 @RequestMapping("/admin")
@@ -27,12 +34,17 @@ public class AdminController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    EmailService emailService;
+
     private final String adminDir = "admin/";
 
     @RequestMapping("/students/hit")
     public String getStudentListHIT(Model model) {
         User clark = userService.findByUsername("clark");
         User harry = userService.findByUsername("harry");
+
+        sendEmailWithoutTemplating();
 
         Programme programme = programmeService.findByName("Hiring in Tech");
 
@@ -72,5 +84,23 @@ public class AdminController {
         adminService.approveStudent2Programme(user, programme);
         model.addAttribute("students", userService.findByUser());
         return adminDir + "listStudents";
+    }
+
+    public void sendEmailWithoutTemplating() {
+        final Email email;
+        try {
+            email = DefaultEmail.builder()
+                    .from(new InternetAddress("cicero@mala-tempora.currunt", "Marco Tullio Cicerone "))
+                    .to(Lists.newArrayList(new InternetAddress("sangseokum@gmail.com", "Sangseok Um")))
+                    .subject("Laelius de amicitia")
+                    .body("Firmamentum autem stabilitatis constantiaeque eius, quam in amicitia quaerimus, fides est.")
+                    .encoding("UTF-8").build();
+
+            emailService.send(email);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
